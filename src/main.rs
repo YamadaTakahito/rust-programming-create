f93b957f23217182cc49b4ec88b10e5be5da16a3
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2020-09-04 21:54:54 +0900

    wip

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+
+    chap1();
+}
+
+fn chap1() {
+    let mut x = 5;
+
+    println!("number is: {}", x);
+    x = 6;
+    println!("number is: {}", x);
+}
