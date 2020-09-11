da93bf77a10a1c5f4c8975534e2fd9033a7f8595
takahito.yamada <d_p.t.a.d.fighter@icloud.com>
2020-09-11 22:23:45 +0900

    add arr

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
 fn main() {
     println!("Hello, world!");
 
-    chap1();
+    // chap1();
+    chap2();
+}
+
+fn chap2() {
+    let i8: i8 = 1;
+    let i16: i16 = 1;
+    let i32: i32 = 1;
+    let i64: i64 = 1;
+    let i128: i128 = 1;
+
+    let u8: u8 = 1;
+    let u16: u16 = 1;
+    let u32: u32 = 1;
+    let u64: u64 = 1;
+    let u128: u128 = 1;
+
+    // let f8: f8 = 10.1;
+    let f32: f32 = 10.1;
+    let f64: f64 = 10.1;
+
+    let s1: String = String::from("hello world");
+    let s2: &str = &s1;
+    let s3: String = s2.to_string();
+
+    let mut t = (1, "2");
+    t.0 = 1;
+    t.1 = "hoge";
+
+    let mut a: [i32; 3] = [0, 1, 2];
+    let b: [i32; 3] = [0; 3];
+    a[1] = b[1];
+    a[2] = b[2];
+    println!("{:?}", &a[1..3]);
 }
 
 fn chap1() {
